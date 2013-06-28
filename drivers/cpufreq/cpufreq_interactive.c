b8bc0b13c39d1ddd68de725e2d527fdbfa211450
anarkia1976 <stefano.villa1976@gmail.com>
2013-06-28 07:16:52 +0200

    cpufreq: interactive: Fix compiling errors after merge

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -35,9 +35,6 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/cpufreq_interactive.h>
 
-#include <linux/syscalls.h>
-#include <linux/highuid.h>
-
 static int active_count;
 
 struct cpufreq_interactive_cpuinfo {
@@ -131,17 +128,6 @@ struct cpufreq_governor cpufreq_gov_interactive = {
 	.max_transition_latency = 10000000,
 	.owner = THIS_MODULE,
 };
-
-#define	AID_SYSTEM	(1000)
-static void dbs_chown(void)
-{
-	int ret;
-
-	ret =
-	sys_chown("/sys/devices/system/cpu/cpufreq/interactive/boostpulse",
-		low2highuid(AID_SYSTEM), low2highgid(0));
-	if (ret)
-		pr_err("sys_chown: boostpulse error: %d", ret);
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
 						  cputime64_t *wall)
@@ -1090,8 +1076,6 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			return -EINVAL;
 
 		mutex_lock(&gov_lock);
-
-		dbs_chown();
 
 		freq_table =
 			cpufreq_frequency_get_table(policy->cpu);
